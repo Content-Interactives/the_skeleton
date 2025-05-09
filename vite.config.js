b8cec3e83fc0c67891b11e54ddeef3518b0fdf6a
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-05-08 19:10:49 -0700

    added repo name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/[YOUR GITHUB REPOSITORY NAME]/',
+  base: '/the_skeleton/',
 })
